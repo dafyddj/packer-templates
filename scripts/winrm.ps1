772b81430ef5ddbaf57406517e4215a47baca48f
Dafydd Jones <dafydd@techneg.it>
2017-04-07 23:29:14 +0100

    changes required to start winrm on win10

--- scripts/winrm.ps1
+++ scripts/winrm.ps1
@@ -1,3 +1,11 @@
-netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
+#netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
+Start-Transcript -Path $env:TEMP\WINRM.PS1.log
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+Set-NetConnectionProfile -NetworkCategory Private
+
+winrm quickconfig -q
 winrm set winrm/config/service/auth '@{Basic="true"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+
+Stop-Transcript

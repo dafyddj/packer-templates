b2e578b44a5ddad24f7f74d87efdbbc37e6e974d
Dafydd Jones <dafydd@techneg.it>
2017-04-11 23:22:17 +0100

    use win10 specific post-reboot script as win10 has different cmdlets to win server

---
+++ scripts/after-reboot-win10.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorAction='SilentlyContinue'
+$ProgressPreference='SilentlyContinue'
+
+Get-WUInstall -WindowsUpdate -AcceptAll -UpdateType Software -IgnoreReboot
+
+Write-Host "Attempting to uninstall features..."
+Get-WindowsOptionalFeature -Online | ? { $_.State -eq 'Disabled' } `
+  | foreach { $_.FeatureName; Disable-WindowsOptionalFeature -Online `
+  -Remove -FeatureName $_.FeatureName
+    Write-Host "Uninstall succeeded!" }

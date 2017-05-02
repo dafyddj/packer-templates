06aec5430cabc9fd5f309601e5b2b0bb2c8ad7c5
Dafydd Jones <dafydd@techneg.it>
2017-05-02 18:35:49 +0100

    add clean-mgr which runs Disk Cleanup tool

---
+++ scripts/clean-mgr.ps1
@@ -0,0 +1,12 @@
+﻿$ProgressPreference='SilentlyContinue'
+
+$regKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches"
+$state = "StateFlags0100"
+
+Get-ChildItem $regKey | ForEach-Object {
+    New-ItemProperty -Path $_.PSPath -Name $state -Value 2 -PropertyType DWord -Force | Out-Null
+}
+
+if (Test-Path "$env:SystemRoot\SYSTEM32\cleanmgr.exe") {
+  Start-Process -Wait cleanmgr -ArgumentList "/sagerun:100"
+}
